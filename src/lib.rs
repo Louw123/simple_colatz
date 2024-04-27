a3cf942263f0a77b662d8bdfd22604a0524f3a40
Louw123 <95922931+Louw123@users.noreply.github.com>
2024-04-26 22:20:12 -0500

    updated Code with a limit

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@
 //! with clean non-terse documentation.
 //! This package allows you to compute the colatz conjecture
 
+use std::i128;
+
 #[doc = r" this function uses a number computes the next number by useing a if condition: If the number doesent have a remainder when its divided by 2 then, divide the number by 2, and return said number, else we multipy and add 1 to the number and return said number."]
 pub fn single_step_colatz(num: usize) -> usize {
     if num % 2 != 0 {
@@ -11,8 +13,9 @@ pub fn single_step_colatz(num: usize) -> usize {
         return num / 2;
     }
 }
-// this function will recursivly call it self until it it reaches one.
-pub fn multi_step_recursive_colatz(num: usize, print: bool) -> usize {
+// this function will recursivly call it self until it it reaches one. Can print out values
+pub fn multi_step_recursive_colatz(num: usize, print: bool) -> i128 {
+    //the bigger the better
     if num == 1 {
         return 1;
     }
@@ -26,18 +29,5 @@ pub fn multi_step_recursive_colatz(num: usize, print: bool) -> usize {
             println!("{}", num);
         }
         return multi_step_recursive_colatz(num / 2, print);
-    }
-}
-pub fn multi_step_nonrecursive_colatz(num: usize, print: bool) -> None {
-    while num != 1 {
-        if num % 2 != 0 {
-            if print == true {
-                println!("{}", num);
-            }
-        } else {
-            if print == true {
-                println!("{}", num);
-            }
-        }
     }
 }
